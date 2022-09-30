d49c43a7a365da702fb472d2aa336742052e9e0b
Riyaldi Hasan <riy.2923@gmail.com>
2022-09-30 13:35:20 +0700

    initial commit

---
+++ app/src/main/java/com/riyaldi/storyapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.riyaldi.storyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyaldi.storyapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}

edb7464d48ea614e866230d903e67a7c6296aedf
Riyaldi Hasan <riy.2923@gmail.com>
2022-10-04 16:31:45 +0700

    add base folder and file

---
+++ app/src/main/java/com/riyaldi/storyapp/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.riyaldi.storyapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riyaldi.storyapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
--- app/src/main/java/com/riyaldi/storyapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/riyaldi/storyapp/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.riyaldi.storyapp
+package com.riyaldi.storyapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle

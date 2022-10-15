09bf9badee92d145f0e92810974ba92481b752ca
Riyaldi Hasan <riy.2923@gmail.com>
2022-10-16 02:22:24 +0700

    add basic ui login and signup

--- app/src/main/java/com/riyaldi/storyapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/riyaldi/storyapp/ui/auth/LoginFragment.kt
@@ -5,20 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.riyaldi.storyapp.R
+import com.riyaldi.storyapp.databinding.FragmentLoginBinding
+import com.riyaldi.storyapp.databinding.FragmentSignUpBinding
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.btLogin.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.mainActivity))
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

66fd26c786619fef06677dc4aeab059a35d32bcc
Riyaldi Hasan <riy.2923@gmail.com>
2022-10-16 02:06:40 +0700

    add navigation from main_activity

---
+++ app/src/main/java/com/riyaldi/storyapp/ui/main/ListStoryFragment.kt
@@ -0,0 +1,38 @@
+package com.riyaldi.storyapp.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.riyaldi.storyapp.R
+import com.riyaldi.storyapp.databinding.FragmentListStoryBinding
+import com.riyaldi.storyapp.databinding.FragmentSignUpBinding
+
+class ListStoryFragment : Fragment() {
+
+    private var _binding: FragmentListStoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentListStoryBinding.inflate(inflater, container, false)
+
+        binding.btDetailStory.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.detailStoryFragment))
+        binding.fabCreateStory.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.createStoryFragment))
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

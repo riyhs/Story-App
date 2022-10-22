e750f9f327ad2fa9f8b6ad156aa74438ad202b1d
Riyaldi Hasan <riy.2923@gmail.com>
2022-10-22 16:30:08 +0700

    add custom edittext

---
+++ app/src/main/java/com/riyaldi/storyapp/ui/customview/CustomEditText.kt
@@ -0,0 +1,53 @@
+package com.riyaldi.storyapp.ui.customview
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.riyaldi.storyapp.R
+
+class CustomEditText: AppCompatEditText, View.OnTouchListener {
+    private lateinit var errorIcon: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        errorIcon = ContextCompat.getDrawable(context, R.drawable.ic_error) as Drawable
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if ((s?.length ?: 0) < 6) {
+                    setError("Password terlalu pendek", errorIcon)
+                    setCompoundDrawablesWithIntrinsicBounds(null, null, errorIcon, null)
+                } else {
+                    setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+    }
+
+    override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+        return false
+    }
+}

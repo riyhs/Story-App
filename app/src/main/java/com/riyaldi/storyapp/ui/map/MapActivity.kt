c175e17c0b8ff78722813f70e790407150531c0e
Riyaldi Hasan <riy.2923@gmail.com>
2022-11-04 14:42:26 +0700

    add many marker

--- app/src/main/java/com/riyaldi/storyapp/ui/map/MapActivity.kt
+++ app/src/main/java/com/riyaldi/storyapp/ui/map/MapActivity.kt
@@ -7,8 +7,10 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import com.riyaldi.storyapp.R
+import com.riyaldi.storyapp.data.remote.response.stories.Story
 import com.riyaldi.storyapp.databinding.ActivityMapBinding
 
 class MapActivity : AppCompatActivity() , OnMapReadyCallback {
@@ -29,8 +31,47 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isIndoorLevelPickerEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.uiSettings.isMapToolbarEnabled = true
+
+        val dicodingSpace = LatLng(-6.8957643, 107.6338462)
+        mMap.addMarker(
+            MarkerOptions()
+                .position(dicodingSpace)
+                .title("Dicoding Space")
+                .snippet("Batik Kumeli No.50")
+        )
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(dicodingSpace, 15f))
+
+        addManyMarker()
+    }
+
+    private val boundsBuilder = LatLngBounds.Builder()
+
+    private fun addManyMarker() {
+        val stories = listOf(
+            Story("","Floating Market Lembang", "id", -6.8168954,107.6151046,"Floating Market Lembang", "photo"),
+            Story("","The Great Asia Africa", "id",-6.8331128,107.6048483,"The Great Asia Africa", "photo"),
+            Story("","Rabbit Town", "id",-6.8668408, 107.608081,"Rabbit Town", "photo"),
+            Story("","Alun-Alun Kota Bandung", "id",-6.9218518,107.6025294,"Alun-Alun Kota Bandung", "photo"),
+            Story("","Orchid Forest Cikole", "id",-6.780725, 107.637409,"Orchid Forest Cikole", "photo"),
+        )
+        stories.forEach { tourism ->
+            val latLng = LatLng(tourism.lat, tourism.lon)
+            mMap.addMarker(MarkerOptions().position(latLng).title(tourism.name))
+            boundsBuilder.include(latLng)
+        }
+
+        val bounds: LatLngBounds = boundsBuilder.build()
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                bounds,
+                resources.displayMetrics.widthPixels,
+                resources.displayMetrics.heightPixels,
+                300
+            )
+        )
     }
 }
